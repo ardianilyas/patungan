c922314e1f83e64721e631a0c84bb4e890c71430
ardianilyas <ardianilyas@gmail.com>
2025-02-25 17:13:18 +0700

    feat: init project

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav class="sticky top-0 px-8 sm:px-10 lg:px-12 py-6 w-full bg-white shadow-md">
+    <div class="flex flex-col md:flex-row md:justify-between md:items-center">
+      <div class="w-full flex justify-between items-center">
+        <Link href="/" class="text-2xl sm:text-3xl font-medium text-neutral-800">Patungan</Link>
+        <button class="md:hidden" @click.prevent="toggleMenu">
+          <MenuIcon v-if="!isOpen" />
+          <XIcon v-else />
+        </button>
+      </div>
+      <ul class="w-full justify-end mt-3 md:mt-0 md:flex flex-col md:flex-row gap-2" :class="isOpen ? 'flex' : 'hidden' ">
+        <li>
+          <NavbarLink :is-active="route().current('home')" :href="route('home')">Home</NavbarLink>
+        </li>
+        <li>
+          <NavbarLink :is-active="false" href="/about">About</NavbarLink>
+        </li>
+        <li v-if="$page.props.auth.user">
+          <NavbarLink :is-active="route().current('dashboard.*')" :href="route('dashboard')">Dashboard</NavbarLink>
+        </li>
+        <li v-if="!$page.props.auth.user">
+          <NavbarLink :is-active="route().current('login.*')" :href="route('login')">Login</NavbarLink>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import { Link } from '@inertiajs/vue3'
+import { MenuIcon, XIcon } from "lucide-vue-next";
+import {ref} from "vue";
+import NavbarLink from "@/components/NavbarLink.vue";
+
+const isOpen = ref(false);
+
+function toggleMenu() {
+  isOpen.value = !isOpen.value
+}
+</script>
---
+++ resources/js/components/NavbarLink.vue
@@ -0,0 +1,20 @@
+<template>
+  <Link :href="href" class="flex hover:text-blue-600" :class="isActive ? 'text-blue-600' : 'text-neutral-800' ">
+    <slot />
+  </Link>
+</template>
+
+<script setup lang="ts">
+import { Link } from '@inertiajs/vue3';
+
+defineProps({
+  href: {
+    required: true,
+    type: String
+  },
+  isActive: {
+    required: true,
+    type: Boolean
+  }
+})
+</script>
