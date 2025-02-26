467abe422216678e679c47f5250b1d743fc3310a
ardianilyas <ardianilyas@gmail.com>
2025-02-26 12:08:40 +0700

    feat: add useAuth composable

---
+++ resources/js/composables/useAuth.js
@@ -0,0 +1,15 @@
+import { usePage } from "@inertiajs/vue3";
+import { computed } from "vue";
+
+export function useAuth() {
+    const page = usePage();
+
+    const user = computed(() => page.props.auth?.user ?? null);
+
+    const isAuthenticated = computed(() => !!user.value)
+
+    return {
+        user,
+        isAuthenticated
+    }
+}
