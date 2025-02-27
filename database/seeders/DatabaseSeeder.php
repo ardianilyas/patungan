040236a6379e7f963c85990f2112b90ead48f0eb
ardianilyas <ardianilyas@gmail.com>
2025-02-27 17:14:35 +0700

    feat: seed topup and transaction

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Topup;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,12 +15,44 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
+        $user = User::factory()->create([
             'name' => 'Ardian Ilyas',
             'email' => 'ardian@patungan.com',
         ]);
-        User::factory(4)->create();
 
-//        Topup::factory(20)->create();
+        $topups = Topup::factory(15)->create();
+
+        foreach ($topups as $topup) {
+
+            $randomDate = collect([
+                now()->subDay(),
+                now()->subDays(rand(1, 15)),
+                now()->subMonths(rand(1, 2))
+            ]);
+
+            $datetime = Carbon::parse($randomDate->random())->toDateTimeString();
+
+            $transaction = $topup->transaction()->create([
+                'user_id' => $user->id,
+                'type' => 'topup',
+                'amount' => $topup->amount,
+                'payment_id' => uniqid(),
+                'payment_method' => fake()->randomElement(['bank transfer', 'qr_code']),
+                'created_at' => $datetime,
+                'updated_at' => $datetime,
+            ]);
+
+            if ($topup->status === 'success') {
+                $user->balance += $topup->amount;
+                $user->save();
+                $topup->paid_at = $datetime;
+                $transaction->status = 'success';
+                $transaction->paid_at = $datetime;
+            } elseif ($topup->status === 'failed') {
+                $transaction->status = 'failed';
+            }
+            $transaction->save();
+            $topup->save();
+        }
     }
 }
